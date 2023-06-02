e123b8777a16e8b8fafff83b4ee662e3268127e3
vintonhuang <vintonhuang965@gmail.com>
2023-06-02 09:56:27 +0800

    feat: 🎸 格式化

--- scripts/version.js
+++ scripts/version.js
@@ -11,5 +11,5 @@ console.log(
   `%c ${pkg.name} %c v${pkg.version} %c`,
   'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
   'background:#1890ff ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
-  'background:transparent'
+  'background:transparent',
 )
