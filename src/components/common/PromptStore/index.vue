237505e6e12700be75fe38f4dc7804749f16bf7a
huangwentao <huangwentao@dianchu.com>
2023-06-27 17:32:08 +0800

    feat: 🎸 移动端角色商店优化

--- src/components/common/PromptStore/index.vue
+++ src/components/common/PromptStore/index.vue
@@ -5,23 +5,26 @@ import {
   NButton,
   NCard,
   NInput,
+  NList,
+  NListItem,
   NModal,
   NPopconfirm,
   NSpace,
   NTabPane,
   NTabs,
+  NThing,
   useMessage,
 } from 'naive-ui'
 import { usePromptStore } from '@/store'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 import { t } from '@/locales'
 
-// interface DataProps {
-//   renderKey: string
-//   renderValue: string
-//   key: string
-//   value: string
-// }
+interface DataProps {
+  renderKey: string
+  renderValue: string
+  key: string
+  value: string
+}
 
 interface Props {
   visible: boolean
@@ -47,7 +50,7 @@ const showModal = ref(false)
 // const importLoading = ref(false)
 const exportLoading = ref(false)
 
-// const searchValue = ref<string>('')
+const searchValue = ref<string>('')
 
 // 移动端自适应相关
 const { isMobile } = useBasicLayout()
@@ -154,14 +157,14 @@ const modifyPromptTemplate = () => {
   changeShowModal('modify')
 }
 
-// const deletePromptTemplate = (row: { key: string; value: string }) => {
-//   promptList.value = [
-//     ...promptList.value.filter(
-//       (item: { key: string; value: string }) => item.key !== row.key,
-//     ),
-//   ] as never
-//   message.success(t('common.deleteSuccess'))
-// }
+const deletePromptTemplate = (row: { key: string; value: string }) => {
+  promptList.value = [
+    ...promptList.value.filter(
+      (item: { key: string; value: string }) => item.key !== row.key,
+    ),
+  ] as never
+  message.success(t('common.deleteSuccess'))
+}
 
 const clearPromptTemplate = () => {
   promptList.value = []
@@ -260,80 +263,22 @@ const exportPromptTemplate = () => {
 // }
 
 // 移动端自适应相关
-// const renderTemplate = () => {
-//   const [keyLimit, valueLimit] = isMobile.value ? [10, 30] : [15, 50]
+const renderTemplate = () => {
+  const [keyLimit, valueLimit] = isMobile.value ? [10, 30] : [15, 50]
 
-//   return promptList.value.map((item: { key: string; value: string }) => {
-//     return {
-//       renderKey:
-//         item.key.length <= keyLimit ? item.key : `${item.key.substring(0, keyLimit)}...`,
-//       renderValue:
-//         item.value.length <= valueLimit
-//           ? item.value
-//           : `${item.value.substring(0, valueLimit)}...`,
-//       key: item.key,
-//       value: item.value,
-//     }
-//   })
-// }
-
-// const pagination = computed(() => {
-//   const [pageSize, pageSlot] = isMobile.value ? [6, 5] : [7, 15]
-//   return {
-//     pageSize,
-//     pageSlot,
-//   }
-// })
-
-// table相关
-// const createColumns = (): DataTableColumns<DataProps> => {
-//   return [
-//     {
-//       title: t('store.title'),
-//       key: 'renderKey',
-//     },
-//     {
-//       title: t('store.description'),
-//       key: 'renderValue',
-//     },
-//     {
-//       title: t('common.action'),
-//       key: 'actions',
-//       width: 100,
-//       align: 'center',
-//       render(row) {
-//         return h(
-//           'div',
-//           { class: 'flex items-center flex-col gap-2' },
-//           {
-//             default: () => [
-//               h(
-//                 NButton,
-//                 {
-//                   tertiary: true,
-//                   size: 'small',
-//                   type: 'info',
-//                   onClick: () => changeShowModal('modify', row),
-//                 },
-//                 { default: () => t('common.edit') },
-//               ),
-//               h(
-//                 NButton,
-//                 {
-//                   tertiary: true,
-//                   size: 'small',
-//                   type: 'error',
-//                   onClick: () => deletePromptTemplate(row),
-//                 },
-//                 { default: () => t('common.delete') },
-//               ),
-//             ],
-//           },
-//         )
-//       },
-//     },
-//   ]
-// }
+  return promptList.value.map((item: { key: string; value: string }) => {
+    return {
+      renderKey:
+        item.key.length <= keyLimit ? item.key : `${item.key.substring(0, keyLimit)}...`,
+      renderValue:
+        item.value.length <= valueLimit
+          ? item.value
+          : `${item.value.substring(0, valueLimit)}...`,
+      key: item.key,
+      value: item.value,
+    }
+  })
+}
 
 watch(
   () => promptList,
@@ -343,16 +288,16 @@ watch(
   { deep: true },
 )
 
-// const dataSource = computed(() => {
-//   const data = renderTemplate()
-//   const value = searchValue.value
-//   if (value && value !== '') {
-//     return data.filter((item: DataProps) => {
-//       return item.renderKey.includes(value) || item.renderValue.includes(value)
-//     })
-//   }
-//   return data
-// })
+const mobileDataSource = computed(() => {
+  const data = renderTemplate()
+  const value = searchValue.value
+  if (value && value !== '') {
+    return data.filter((item: DataProps) => {
+      return item.renderKey.includes(value) || item.renderValue.includes(value)
+    })
+  }
+  return data
+})
 // 打开的时候需要重置一下当前的选择卡片
 watch(
   () => show,
@@ -371,69 +316,65 @@ const changeTab = async (value: number) => {
 <template>
   <NModal v-model:show="show" style="width: 90%; max-width: 1400px" title="角色商店" preset="card">
     <div class="space-y-4">
-      <NTabs type="segment">
-        <NTabPane name="local" tab="当前选中的tab分类，可以在输入框的时候自动获取分类角色">
-          <div
-            class="flex gap-3 mb-4"
-            :class="[isMobile ? 'flex-col' : 'flex-row justify-between']"
+      <div
+        class="flex gap-3 mb-4"
+        :class="[isMobile ? 'flex-col' : 'flex-row justify-between']"
+      >
+        <div class="flex items-center space-x-4">
+          <NButton type="primary" size="small" @click="changeShowModal('add')">
+            {{ $t("common.add") }}
+          </NButton>
+          <NButton size="small" @click="changeShowModal('local_import')">
+            {{ $t("common.import") }}
+          </NButton>
+          <NButton
+            size="small"
+            :loading="exportLoading"
+            @click="exportPromptTemplate()"
           >
-            <div class="flex items-center space-x-4">
-              <NButton type="primary" size="small" @click="changeShowModal('add')">
-                {{ $t("common.add") }}
+            {{ $t("common.export") }}
+          </NButton>
+          <NPopconfirm @positive-click="clearPromptTemplate">
+            <template #trigger>
+              <NButton size="small">
+                {{ $t("common.clear") }}
               </NButton>
-              <NButton size="small" @click="changeShowModal('local_import')">
-                {{ $t("common.import") }}
-              </NButton>
-              <NButton
-                size="small"
-                :loading="exportLoading"
-                @click="exportPromptTemplate()"
-              >
-                {{ $t("common.export") }}
-              </NButton>
-              <NPopconfirm @positive-click="clearPromptTemplate">
-                <template #trigger>
-                  <NButton size="small">
-                    {{ $t("common.clear") }}
-                  </NButton>
-                </template>
-                {{ $t("store.clearStoreConfirm") }}
-              </NPopconfirm>
-            </div>
-            <!-- <div class="flex items-center">
+            </template>
+            {{ $t("store.clearStoreConfirm") }}
+          </NPopconfirm>
+        </div>
+        <!-- <div class="flex items-center">
               <NInput v-model:value="searchValue" style="width: 100%" />
             </div> -->
-          </div>
-          <NTabs
-            :on-update:value="changeTab"
-            :value="promptStore.selectIndex"
-          >
-            <NTabPane
-              v-for="element in promptStore.CategoryList"
-              :key="element.scene_id"
-              :name="element.scene_id"
-              :tab="element.scene_name"
+      </div>
+      <NTabs
+        :on-update:value="changeTab"
+        :value="promptStore.selectIndex"
+      >
+        <NTabPane
+          v-for="element in promptStore.CategoryList"
+          :key="element.scene_id"
+          :name="element.scene_id"
+          :tab="element.scene_name"
+        >
+          <div v-if="!isMobile" class="typeContainer">
+            <NCard
+              v-for="item in promptList"
+              :key="item.key"
+              class="card-item"
+              :title="item.key"
+              hoverable
             >
-              <div class="typeContainer">
-                <NCard
-                  v-for="item in promptList"
-                  :key="item.key"
-                  class="card-item"
-                  :title="item.key"
-                  hoverable
-                >
-                  {{ item.value }}
-                  <template #action>
-                    <NButton @click="changeShowModal('modify', item)">
-                      编辑
-                    </NButton>
-                  </template>
-                </NCard>
-              </div>
-            </NTabPane>
-          </NTabs>
-          <!-- <NList v-if="isMobile" style="max-height: 400px; overflow-y: auto">
-            <NListItem v-for="(item, index) of dataSource" :key="index">
+              {{ item.value }}
+              <template #action>
+                <NButton @click="changeShowModal('modify', item)">
+                  编辑
+                </NButton>
+              </template>
+            </NCard>
+          </div>
+          <NList v-else style="max-height: 400px; overflow-y: auto">
+            <NListItem v-for="(item, index) of mobileDataSource" :key="index">
               <NThing :title="item.renderKey" :description="item.renderValue" />
               <template #suffix>
                 <div class="flex flex-col items-center gap-2">
@@ -456,54 +397,8 @@ const changeTab = async (value: number) => {
                 </div>
               </template>
             </NListItem>
-          </NList> -->
+          </NList>
         </NTabPane>
-        <!-- <NTabPane name="download" :tab="$t('store.online')">
-          <p class="mb-4">
-            {{ $t("store.onlineImportWarning") }}
-          </p>
-          <div class="flex items-center gap-4">
-            <NInput v-model:value="downloadURL" placeholder="" />
-            <NButton
-              strong
-              secondary
-              :disabled="downloadDisabled"
-              :loading="importLoading"
-              @click="downloadPromptTemplate()"
-            >
-              {{ $t("common.download") }}
-            </NButton>
-          </div>
-          <NDivider />
-          <div class="max-h-[360px] overflow-y-auto space-y-4">
-            <NCard
-              v-for="info in promptRecommendList"
-              :key="info.key"
-              :title="info.key"
-              :bordered="true"
-              embedded
-            >
-              <p
-                class="overflow-hidden text-ellipsis whitespace-nowrap"
-                :title="info.desc"
-              >
-                {{ info.desc }}
-              </p>
-              <template #footer>
-                <div class="flex items-center justify-end space-x-4">
-                  <NButton text>
-                    <a :href="info.url" target="_blank">
-                      <SvgIcon class="text-xl" icon="ri:link" />
-                    </a>
-                  </NButton>
-                  <NButton text @click="setDownloadURL(info.downloadUrl)">
-                    <SvgIcon class="text-xl" icon="ri:add-fill" />
-                  </NButton>
-                </div>
-              </template>
-            </NCard>
-          </div>
-        </NTabPane> -->
       </NTabs>
     </div>
   </NModal>
